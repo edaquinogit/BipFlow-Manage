da3208eb428bd746c18ffc78a38a5e8b814f40ff
Ednaldo <ednaldoaquino87@gmail.com>
2026-03-02 16:21:34 -0300

    feat: setup media files and update product serialization with slug fields

--- bipdelivery/api/serializers.py
+++ bipdelivery/api/serializers.py
@@ -2,8 +2,12 @@ from rest_framework import serializers
 from .models import Product, Category
 
 class ProductSerializer(serializers.ModelSerializer):
-    category_name = serializers.ReadOnlyField(source='category.name')
+    # Isso permite que o Vue envie o NOME da categoria e o Django encontre o ID sozinho
+    category = serializers.SlugRelatedField(
+        slug_field='name', 
+        queryset=Category.objects.all()
+    )
 
     class Meta:
         model = Product
-        fields = ['id', 'name', 'description', 'price', 'category', 'category_name', 'is_available', 'image']
+        fields = ['id', 'name', 'description', 'price', 'category', 'is_available', 'image']
--- bipdelivery/core/urls.py
+++ bipdelivery/core/urls.py
@@ -1,11 +1,21 @@
 from django.contrib import admin
 from django.urls import path, include
-# Importação direta do SimpleJWT
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Garanta que estas duas linhas existam:
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/', include('api.urls')), # <--- ESSA LINHA CONECTA TUDO
+    
+    # Suas outras rotas (produtos, etc)
+    path('api/', include('api.urls')), 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
